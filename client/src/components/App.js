10c4195c4c700694901986c9039c060851ad2f2d
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2022-04-01 22:47:05 -0700

    initial commit

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Chat APP</header>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
